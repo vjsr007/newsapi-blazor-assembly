2a5df9170a9d760aa769210eb0aeb2103b28138a
TH658FY <victor.sanchez@ey.com>
2021-08-11 14:45:38 -0500

    appsettings

---
+++ src/Constants/NewsApiConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Constants
+{
+    public static class NewsApiConstants
+    {
+        public const string ARTICLES_ENDPOINT = "{0}everything?{1}&apiKey={2}";
+        public const string SOURCE_ENDPOINT = "{0}top-headlines/sources?apiKey={1}";
+    }
+}
