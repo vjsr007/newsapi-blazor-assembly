0bd940a65c9d16fec8a74aa3cc412ac924918211
Vj <vjsanchezr@hotmail.com>
2021-08-11 00:58:50 -0500

    states

--- src/Stored/NewsState.cs
+++ src/Stored/NewsState.cs
@@ -6,6 +6,20 @@ namespace Client.Stored
 {
     public class NewsState
     {
+        #region Topic
+        public string TopicSelected { get; private set; }
+
+        public event Action OnChangeTopic;
+
+        public void SetTopic(string topic)
+        {
+            TopicSelected = topic;
+            NotifyTopicChanged();
+        }
+        private void NotifyTopicChanged() => OnChangeTopic?.Invoke();
+
+        #endregion
+
         #region Sources
         public ICollection<DropdownOption> SourceSelected { get; private set; }
 
@@ -17,6 +31,48 @@ namespace Client.Stored
             NotifySourcesChanged();
         }
         private void NotifySourcesChanged() => OnChangeSources?.Invoke();
+
+        #endregion
+
+        #region Dates
+        public ICollection<DropdownOption> DateSelected { get; private set; }
+
+        public event Action OnChangeDate;
+
+        public void SetDate(ICollection<DropdownOption> dates)
+        {
+            DateSelected = dates;
+            NotifyDateChanged();
+        }
+        private void NotifyDateChanged() => OnChangeDate?.Invoke();
+
+        #endregion
+
+        #region Languages
+        public ICollection<DropdownOption> LanguageSelected { get; private set; }
+
+        public event Action OnChangeLanguage;
+
+        public void SetLanguage(ICollection<DropdownOption> languages)
+        {
+            LanguageSelected = languages;
+            NotifyLanguageChanged();
+        }
+        private void NotifyLanguageChanged() => OnChangeLanguage?.Invoke();
+
+        #endregion
+
+        #region SortBy
+        public ICollection<DropdownOption> SortBySelected { get; private set; }
+
+        public event Action OnChangeSortBy;
+
+        public void SetSortBy(ICollection<DropdownOption> sortBy)
+        {
+            SortBySelected = sortBy;
+            NotifySortByChanged();
+        }
+        private void NotifySortByChanged() => OnChangeSortBy?.Invoke();
 
         #endregion
     }
